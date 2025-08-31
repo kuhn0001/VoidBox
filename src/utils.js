518c4203493bcdf35257a95c994afc1e31d50182
kuhn8tor <123999526+kuhn8tor@users.noreply.github.com>
2025-08-31 09:57:08 -0400

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+// utils.js
+export const W=960, H=540;
+export const clamp=(v,a,b)=>Math.max(a,Math.min(b,v));
+export const rand=(a=1,b=0)=>b+(a-b)*Math.random();
+export const rndi=(a,b)=>a+Math.floor(Math.random()*(b-a+1));
+export const collide=(a,b)=>{ const dx=a.x-b.x, dy=a.y-b.y; const rr=(a.r||0)+(b.r||0); return dx*dx+dy*dy<rr*rr; };
